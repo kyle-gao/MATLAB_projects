61e29f9ac7d7672eb6d23c8082393bfc46797f5e
kyle-gao <67448832+kyle-gao@users.noreply.github.com>
2020-09-05 11:38:26 -0400

    Add files via upload

--- KNN_from_scratch/main.m
+++ KNN_from_scratch/main.m
@@ -7,16 +7,23 @@ Y_train = Y(1:120,:);
 X_val = X(121:end,:);
 Y_val = Y(121:end,:);
 
-Model = fitcknn(X_train,Y_train,'NumNeighbors',5,'Standardize',1)
+k = 3 %small k tend to overfit, large k tend to underfit. 
+%k = 1 perfectly fits training set. 
+
+
+Model = fitcknn(X_train,Y_train,'NumNeighbors',3,'Standardize',1)
 [label,score,cost]= predict(Model,X_val);
 
 s = size(label);
 accuracy = sum(cellfun(@isequal, Y_val, label))/s(1);
 disp(accuracy)
-%accuracy=0.9667
+%accuracy=0.9333
 
-mylabels = myKNN(X_train,Y_train, X_val, 5);
+mylabels = myKNN(X_train,Y_train, X_val, 3);
 accuracy = sum(cellfun(@isequal, Y_val, mylabels))/s(1);
 disp(accuracy)
-%accuracy=0.9667
+%accuracy=0.9333
 
+save('X_val.mat', 'X_val');
+save('label.mat', 'label');
+save('mylabels.mat', 'mylabels');
